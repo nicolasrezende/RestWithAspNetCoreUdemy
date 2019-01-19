0abe499815d8d1653b0c322e21288d10da690ea2
NicolasSR <nicolassilva114@gmail.com>
2019-01-19 21:57:08 -0200

    Working with Data Contract

--- RestWithAspNetCoreUdemy/RestWithAspNetCoreUdemy/Data/VO/BookVO.cs
+++ RestWithAspNetCoreUdemy/RestWithAspNetCoreUdemy/Data/VO/BookVO.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace RestWithAspNetCoreUdemy.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        [DataMember(Order = 1, Name = "codigo")]
         public long Id { get; set; }
+
+        [DataMember(Order = 2, Name = "titulo")]
         public string Title { get; set; }
+
+        [DataMember(Order = 3, Name = "autor")]
         public string Author { get; set; }
+
+        [DataMember(Order = 5, Name = "preco")]
         public decimal Price { get; set; }
+
+        [DataMember(Order = 4, Name = "dataLancamento")]
         public DateTime LaunchDate { get; set; }
     }
 }
